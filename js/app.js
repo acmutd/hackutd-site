6a986af410317bf81fce7e146a13ae7a9b3a6e24
matthewle2020 <mml160230@utdallas.edu>
2018-11-22 12:01:07 -0600

    Changed particle effect

--- js/app.js
+++ js/app.js
@@ -1,45 +1,26 @@
-particlesJS("particles-js", {"particles":{"number":{"value":160,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"star","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"bottom-left","random":false,"straight":true,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":100,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":300,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
+//particlesJS("particles-js", {"particles":{"number":{"value":160,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"star","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"bottom-left","random":false,"straight":true,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":100,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":300,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
-/*var count_particles, stats, update; 
-stats = new Stats; 
-stats.setMode(0); 
-stats.domElement.style.position = 'absolute'; 
-stats.domElement.style.left = '0px'; 
-stats.domElement.style.top = '0px'; 
-document.body.appendChild(stats.domElement); 
-count_particles = document.querySelector('.js-count-particles'); 
-update = function() { 
-    stats.begin(); 
-    stats.end(); 
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { 
-        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; 
-    } 
-    requestAnimationFrame(update); 
-}; 
-requestAnimationFrame(update);;
-*/
-/*
 particlesJS('particles-js', 
 	{
 	  "particles": {
 	    "number": {
-	      "value": 40,
+	      "value": 30,
 	      "density": {
 	        "enable": true,
-	        "value_area": 800
+	        "value_area": 750
 	      }
 	    },
 	    "color": {
-	      "value": "#ef626c"
+	      "value": ["8f2e3e","#ef626c"]
 	    },
 	    "shape": {
-	      "type": "circle",
+	      "type": ["polygon","triangle","circle","edge"],
 	      "stroke": {
 	        "width": 0,
 	        "color": "#000000"
 	      },
 	      "polygon": {
-	        "nb_sides": 5
+	        "nb_sides": 6
 	      },
 	      "image": {
 	        "src": "img/github.svg",
@@ -58,12 +39,12 @@ particlesJS('particles-js',
 	      }
 	    },
 	    "size": {
-	      "value": 112,
+	      "value": 100,
 	      "random": true,
 	      "anim": {
 	        "enable": false,
 	        "speed": 40,
-	        "size_min": 0.1,
+	        "size_min": 20,
 	        "sync": false
 	      }
 	    },
@@ -73,7 +54,7 @@ particlesJS('particles-js',
 	    "move": {
 	      "enable": true,
 	      "speed": 1.5,
-	      "direction": "top",
+	      "direction": "none",
 	      "random": false,
 	      "straight": false,
 	      "out_mode": "out",
@@ -89,11 +70,11 @@ particlesJS('particles-js',
 	    "detect_on": "canvas",
 	    "events": {
 	      "onhover": {
-	        "enable": false,
-	        "mode": "repulse"
+	        "enable": true,
+	        "mode": "bubble"
 	      },
 	      "onclick": {
-	        "enable": true,
+	        "enable": false,
 	        "mode": "remove"
 	      },
 	      "resize": true
@@ -106,11 +87,11 @@ particlesJS('particles-js',
 	        }
 	      },
 	      "bubble": {
-	        "distance": 400,
-	        "size": 40,
+	        "distance": 100,
+	        "size": 0,
 	        "duration": 2,
 	        "opacity": 8,
-	        "speed": 3
+	        "speed": 5
 	      },
 	      "repulse": {
 	        "distance": 200,
@@ -128,8 +109,6 @@ particlesJS('particles-js',
 	}
 
 );
-
-*/
 
 /*
 $(document).ready(function () {
