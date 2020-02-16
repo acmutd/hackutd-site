40dada0f7fd27d587c8a2e854d7206aa30f849e7
Harsha Srikara <harshasrikara@gmail.com>
2020-02-15 18:29:48 -0600

    Added browser compatibility, mobile compatibility
    Dynamic sizing changes for different sized screens and resolutions, added it through jquery and a couple of other js scripts. Modified to look perfect on mobile

---
+++ archive/19f/js/init.js
@@ -0,0 +1,11 @@
+(function($) {
+  $(function() {
+    $('select').material_select();
+    $('.slider').slider();
+    $(".button-collapse").sideNav();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
