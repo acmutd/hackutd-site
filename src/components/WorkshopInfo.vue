e4834d58a6c8e646bafeb89088059369bd1170aa
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-30 21:20:56 -0700

    Improve WorkshopInfo styles
    Add temporary horizontal line below each workshop

--- src/components/WorkshopInfo.vue
+++ src/components/WorkshopInfo.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="workshop">
-    <h1 class="workshop-title text-lg">{{ title }}</h1>
+    <h1 class="workshop-title text-xl font-bold">{{ title }}</h1>
     <div class="workshop-date text-sm">{{ date }}</div>
     <div class="workshop-description">{{ description }}</div>
     <div class="workshop-tags">{{ tags }}</div>
+    <hr class="mt-4">
   </div>
 </template>
 
