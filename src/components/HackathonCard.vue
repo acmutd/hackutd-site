3c6585c87d90f62104d332c1301f1c0ca3294e11
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-30 21:23:24 -0700

    Add old event information to HackUTD history

--- src/components/HackathonCard.vue
+++ src/components/HackathonCard.vue
@@ -1,23 +1,23 @@
 <!-- A card with info about a past HackUTD event. -->
 <template>
-  <div class="bg-white rounded-lg p-6">
-    <g-image :src="eventImage" :alt="eventName" />
+  <div class="rounded-lg bg-white shadow-md hover:shadow-lg p-6">
+    <g-image :src="eventImage" :alt="title" />
     <div class="text-center md:text-left">
-      <h2 class="text-lg">{{ eventName }}</h2>
-      <div class="text-purple-500">{{ date }}</div>
-      <div class="text-gray-600">{{ attendees }} hackers</div>
+      <h2 class="text-lg text-black">{{ title }}</h2>
+      <div class="text-black">{{ date }}</div>
+      <div class="text-black">{{ attendees }} hackers</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: [
-    'info',
-    'eventName',
-    'date',
-    'attendees',
-    'eventImage',
-  ]
+  props: {
+    title: String,
+    description: String,
+    date: String,
+    attendees: Number,
+    eventImage: String,
+  },
 };
 </script>
--- src/pages/History.vue
+++ src/pages/History.vue
@@ -1,22 +1,46 @@
 <template>
   <Layout>
-    <div class="hero">
+    <section class="hero max-w-4xl mx-auto mt-4 p-4">
+      <h1 class="text-4xl text-center font-bold pt-8">How We Began</h1>
       <div class="hackutd-info">
         <p>
           HackUTD started in 2014 with the goal of bringing together people to build stuff.
-        </p>
-        <p>
           Since 2014, HackUTD has grown to be the largest hackathon in northern Texas.
         </p>
       </div>
-    </div>
-    <div class="past-hackathons">
-      <hackathon-card></hackathon-card>
-      <hackathon-card></hackathon-card>
-      <hackathon-card></hackathon-card>
-    </div>
+    </section>
+    <section class="container max-w-4xl mx-auto">
+      <h1 class="text-4xl text-center font-bold pt-8">Past Events</h1>
+      <div class="past-hackathons">
+        <hackathon-card
+        class="m-4"
+        v-for="edge in $page.events.edges" :key="edge.id"
+        :title="edge.node.title"
+        :attendees="attendees"
+        :description="edge.node.description">
+      </hackathon-card>
+      </div>
+    </section>
   </Layout>
 </template>
+
+<page-query>
+query HackUTDHistory {
+  events: allHackathonEvents {
+    edges {
+      node {
+        title
+        description
+        date
+        imageName
+        venue
+        attendees
+        layout  
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 import HackathonCard from '../components/HackathonCard.vue';
@@ -25,10 +49,10 @@ export default {
   metaInfo: {
     title: 'HackUTD - Organization Information',
   },
-  props: [
-
-  ]
-}
+  components: {
+    'hackathon-card': HackathonCard,
+  },
+};
 </script>
 
 <style>
