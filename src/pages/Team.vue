1d805a9c3f657e8767d47f8e85fb680a4b66c5c9
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-30 21:18:58 -0700

    Use actual data for organizer info

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -1,27 +1,45 @@
 <template>
   <Layout>
-    <section class="px-6 text-left md:text-center xl:text-left max-w-2xl md:max-w-3xl mx-auto">
+    <section class="px-4 text-left md:text-center xl:text-left max-w-2xl md:max-w-6xl mx-auto">
       <h1 class="text-2xl">Current Team</h1>
-      <div class="team-members">
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
+      <div class="team-members md:grid md:gap-4 md:grid-cols-2">
+        <team-member
+          v-for="edge in $page.events.edges"
+          :name="edge.node.name"
+          :position="edge.node.position"
+          :bio="edge.node.bio"
+          :image-url="imageName"
+          :github="edge.node.github"
+          :website="edge.node.website"
+          :linkedin="edge.node.linkedin"
+          :key="edge.node.id"
+        ></team-member>
       </div>
     </section>
-    <section class="px-6 text-left md:text-center xl:text-left max-w-2xl md:max-w-3xl mx-auto">
+    <!-- <section class="px-6 text-left md:text-center xl:text-left max-w-2xl md:max-w-3xl mx-auto">
       <h1 class="text-2xl">Past Team</h1>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-        <team-member name="Camden Squire" image-url="https://www.acmutd.co/png/team-hackutd-camden.jpg" position="HackUTD Officer" bio="I organize things. When I'm not organizing things, I'm off programming and being a CS god."></team-member>
-    </section>
+    </section> -->
   </Layout>
 </template>
+
+<page-query>
+query {
+  events: allOrganizers {
+    edges {
+      node {
+        id
+        name
+        position
+        bio
+        imageName
+        github
+        website
+        linkedin
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 import OrganizerInfo from "../components/OrganizerInfo.vue";
@@ -29,7 +47,15 @@ import OrganizerInfo from "../components/OrganizerInfo.vue";
 export default {
   components: {
     "team-member": OrganizerInfo
-  }
+  },
+  computed: {
+    pastTeam() {
+      return [];
+    },
+  },
+  data() {
+    return {};
+  },
 };
 </script>
 
