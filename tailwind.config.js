6884b8adbb818eeaf649793a2d52cd49124f209e
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-30 18:46:44 -0700

    Begin creating dark/light theme styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
+  purge: false,
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        light: { raw: "(prefers-color-scheme: light)" },
+        dark: { raw: "(prefers-color-scheme: dark)" }
+      },
+    },
+    colors: {
+      black: '#444444',
+      white: '#FAFAFA',
+    },
   },
   variants: {},
-  plugins: [],
-}
+  plugins: [
+    function darkModePlugin({ addBase, config }) {
+      addBase({
+        body: {
+          color: config("theme.colors.black"),
+          backgroundColor: config("theme.colors.white"),
+        },
+        "@screen dark": {
+          body: {
+            color: config("theme.colors.white"),
+            backgroundColor: config("theme.colors.black"),
+          },
+        },
+      });
+    },
+  ],
+};
