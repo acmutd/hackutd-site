699ac2384071940bb594b0fdb4eed5feb5ae1afe
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-30 21:11:01 -0700

    Update README

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,16 +1,42 @@
-// Server API makes it possible to hook into various parts of Gridsome
-// on server-side and add custom data to the GraphQL data layer.
-// Learn more: https://gridsome.org/docs/server-api/
+const XLSX = require('xlsx');
 
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+const DATA_PATH = 'data/organizers.xlsx';
+
+const ORGANIZERS_SHEET_NAME = 'Organizers';
+const WORKSHOPS_SHEET_NAME = 'Workshops';
+const EVENTS_SHEET_NAME = 'Events';
 
 module.exports = function (api) {
+  const workbook = XLSX.readFile(DATA_PATH);
+  const organizers = XLSX.utils.sheet_to_json(workbook.Sheets[ORGANIZERS_SHEET_NAME], { raw: true, defval: null });
+  const events = XLSX.utils.sheet_to_json(workbook.Sheets[EVENTS_SHEET_NAME], { raw: true, defval: null });
+  const workshops = XLSX.utils.sheet_to_json(workbook.Sheets[WORKSHOPS_SHEET_NAME], { raw: true, defval: null });
+
   api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+    const organizerCollection = addCollection('Organizers');
+    organizers.forEach((organizer, index) => {
+      organizerCollection.addNode({
+        id: index,
+        ...organizer,
+      });
+    });
+    const eventsCollection = addCollection('HackathonEvents');
+    events.forEach((event, index) => {
+      eventsCollection.addNode({
+        id: index,
+        ...event,
+      });
+    });
+    const workshopsCollection = addCollection('Workshops');
+    workshops.forEach((workshop, index) => {
+      workshopsCollection.addNode({
+        id: index,
+        ...workshop,
+      });
+    });
+  });
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
+  });
 }
