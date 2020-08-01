79628376ac597a27ded4c2d7709a01d6c9c708e5
Willie Chalmers III <willieechalmers@gmail.com>
2020-08-01 12:26:07 -0700

    Update header layout
    TODO: Use responsive layout to limit length of contents

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,21 @@
 <template>
   <header class="dark:header-dark block p-4 md:flex md:h-16">
-    <div class="logo-container block md:inline-block">
-      <!-- <div class="logo">
-        <g-image src="~/assets/hackutd-horizontal-logo.svg"></g-image>
-      </div> -->
-      <div class="site-title block md:inline-block text-3xl">
-        <g-link to="/">HackUTD</g-link>
-        </div>
+    <div class="max-w-4xl mx-auto">
+      <div class="logo-container block md:inline-block">
+        <div class="site-title block md:inline-block text-3xl">
+          <g-link to="/">
+            <g-image src="../assets/logo-horizontal-white.svg" class="logo"></g-image>
+          </g-link>
+          </div>
+      </div>
+      <nav class="nav">
+        <g-link class="nav-item block md:inline-block px-4 h-full" to="/">Home</g-link>
+        <g-link class="nav-item block md:inline-block px-4 h-full" to="/history">History</g-link>
+        <g-link class="nav-item block md:inline-block px-4 h-full" to="/events">Events</g-link>
+        <g-link class="nav-item block md:inline-block px-4 h-full" to="/team">Team</g-link>
+        <g-link class="nav-item block md:inline-block px-4 h-full" to="/contact">Contact</g-link>
+      </nav>
     </div>
-    <!-- <g-link to="/">{{ $static.metadata.siteName }}</g-link> -->
-    <!-- <nav class="nav overflow-scroll"> -->
-    <nav class="nav">
-      <g-link class="nav-item block md:inline-block px-4 h-full" to="/">Home</g-link>
-      <g-link class="nav-item block md:inline-block px-4 h-full" to="/history">History</g-link>
-      <g-link class="nav-item block md:inline-block px-4 h-full" to="/events">Events</g-link>
-      <g-link class="nav-item block md:inline-block px-4 h-full" to="/team">Team</g-link>
-      <g-link class="nav-item block md:inline-block px-4 h-full" to="/contact">Contact</g-link>
-    </nav>
   </header>
 </template>
 
@@ -39,5 +38,10 @@ header {
   align-items: center;
   @apply text-white;
   @apply bg-black;
+}
+
+.logo {
+  @apply h-12;
+  width: 200px;
 }
 </style>
