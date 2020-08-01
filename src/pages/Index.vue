18504f5cacc01de3ccf64558d4f79c6bf2a0ffe8
Willie Chalmers III <willieechalmers@gmail.com>
2020-08-01 12:24:06 -0700

    Recreate site landing

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,76 +1,30 @@
 <template>
-  <home-layout>
-    <template v-slot:header>
-      <section id="hero" class="hero h-full flex flex-col">
-        <div class="md:flex flex-grow max-w-4xl">
-          <div class="w-full flex flex-col justify-center">
-            <g-image src="~/assets/hackutd-logo-white.png" class="m-auto" />
-          </div>
-          <div class="w-full text-left md:my-auto">
-            <div class="m-4">
-              <div class="md:text-5xl text-bold">Hack.</div>
-              <div class="md:text-5xl text-bold">Build.</div>
-              <div class="md:text-5xl text-bold">Learn.</div>
-              <div class="cta">
-                <button
-                  class="rounded-lg px-4 md:px-5 py-3 md:py-4 bg-teal-500 hover:bg-teal-600 md:text-lg xl:text-base text-white font-semibold leading-tight shadow-md"
-                >
-                  <span class="bg-orange-500">Apply to HackUTD VII</span>
-                </button>
-              </div>
-            </div>
-          </div>
+  <div>
+    <section class="hero h-screen">
+      <div class="md:m-auto md:max-w-3xl md:flex">
+        <div class="flex-1">
+          <img src="../assets/logo-square-white.svg" alt="HackUTD logo" class="">
         </div>
-        <div class="flex justify-center h-8 m-2">
-          <div>
-            <i class="material-icons h-10 w-10 m-2">arrow_down</i>
-            More
-          </div>
-        </div>
-      </section>
-    </template>
-    <section
-      id="about"
-      class="py-4 my-4 text-left md:text-center xl:text-left max-w-2xl md:max-w-3xl mx-auto"
-    >
-      <div class="mission">
-        <h1 class="text-2xl">Mission</h1>
-        <div class="flex">
-          <div class="flex-grow">
-            <p>
-              HackUTD brings together students from across northern Texas and even the world to learn
-              and grow as developers.
-              HackUTD hosts technical workshops in preparation for its hackathons
-              held in the fall and spring along with on-site events.
-            </p>
-            <g-link class="text-xl font-bold" to="/history">> See our events</g-link>
-        </div>
+        <div class="flex-1 text-6xl font-display font-bold px-4">
+          <span class="md:block">Hack.</span>
+          <span class="md:block">Build.</span>
+          <span class="md:block">Learn.</span>
+          <a class="rounded-md shadow-md font-sans font-normal text-lg bg-orange p-4 text-black focus:shadow-lg hover:shadow-lg" href="/">Sign up for our newsletter</a>
         </div>
       </div>
-    </section>
-    <section id="team"
-    class="py-4 my-4 text-left md:text-center xl:text-left max-w-2xl md:max-w-3xl mx-auto">
-      <h1 class="text-2xl">Team</h1>
-      <div class="flex">
-        <div class="flex-grow">
-          <p>
-            The HackUTD team is a set of dedicated students at The University of
-            Texas at Dallas. As part of the student chapter for the Assocation for
-            Computing Machinery at UT Dallas, the organizers are responsible for
-            making the hacker experience as engaging as it can be.
-          </p>
-          <g-link class="text-xl font-bold" to="/team">> See the team</g-link>
-        </div>
+      <div class="text-center">
+        <span class="material-icons">arrow_down</span>
+        <span class="text-lg mx-2">Learn more</span>
       </div>
     </section>
-  </home-layout>
+  </div>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: "Organization Information"
-  }
+    title: 'Home',
+  },
 };
 </script>
 
