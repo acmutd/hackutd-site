acd536eb7ec59338044cbdc09e8a500a13da6ed6
Willie Chalmers III <willie@williecubed.me>
2020-08-02 08:58:40 -0700

    Update header styles
    TODO: Center logo on mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,19 @@
 <template>
-  <header class="dark:header-dark block p-4 md:flex md:h-16">
-    <div class="max-w-4xl mx-auto">
-      <div class="logo-container block md:inline-block">
-        <div class="site-title block md:inline-block text-3xl">
+  <header class="dark:header-dark block p-4 md:flex md:flex-col md:h-20">
+    <div class="max-w-4xl mx-auto md:flex">
+      <div class="logo-container text-center block md:inline-block md:flex-grow-0">
+        <div class="site-title md:inline text-3xl">
           <g-link to="/">
             <g-image src="../assets/logo-horizontal-white.svg" class="logo"></g-image>
           </g-link>
-          </div>
+        </div>
       </div>
-      <nav class="nav">
-        <g-link class="nav-item block md:inline-block px-4 h-full" to="/">Home</g-link>
-        <g-link class="nav-item block md:inline-block px-4 h-full" to="/history">History</g-link>
-        <g-link class="nav-item block md:inline-block px-4 h-full" to="/events">Events</g-link>
-        <g-link class="nav-item block md:inline-block px-4 h-full" to="/team">Team</g-link>
-        <g-link class="nav-item block md:inline-block px-4 h-full" to="/contact">Contact</g-link>
+      <nav class="nav block md:justify-center md:inline-block md:my-auto">
+        <g-link class="nav-item px-4 h-full" to="/">Home</g-link>
+        <g-link class="nav-item px-4 h-full" to="/history">History</g-link>
+        <g-link class="nav-item px-4 h-full" to="/events">Events</g-link>
+        <g-link class="nav-item px-4 h-full" to="/team">Team</g-link>
+        <g-link class="nav-item px-4 h-full" to="/contact">Contact</g-link>
       </nav>
     </div>
   </header>
@@ -25,23 +25,35 @@
   @apply text-black;
 }
 
+.nav {
+  @apply mt-4;
+  overflow-x: scroll;
+}
+
+@screen md {
+  .nav {
+    @apply my-auto;
+    overflow: hidden;
+  }
+}
+
 .nav-item {
   @apply font-bold text-lg;
   @apply px-4;
+  @apply font-display;
 }
 
 header {
+  @apply sticky top-0;
   @apply shadow-lg;
-  position: sticky;
-  top: 0;
-  justify-content: space-between;
-  align-items: center;
   @apply text-white;
   @apply bg-black;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .logo {
   @apply h-12;
-  width: 200px;
+  width: 240px;
 }
 </style>
