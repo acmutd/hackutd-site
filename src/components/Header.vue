a3be50b7f4829425a36bc10a0ef0343d13a0c182
David Richey <darichey1@gmail.com>
2020-08-02 03:16:15 -0500

    Header, home page tweaks, dead code removal

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,47 +1,11 @@
 <template>
-  <header class="dark:header-dark block p-4 md:flex md:h-16">
-    <div class="max-w-4xl mx-auto">
-      <div class="logo-container block md:inline-block">
-        <div class="site-title block md:inline-block text-3xl">
-          <g-link to="/">
-            <g-image src="../assets/logo-horizontal-white.svg" class="logo"></g-image>
-          </g-link>
-          </div>
-      </div>
-      <nav class="nav">
-        <g-link class="nav-item block md:inline-block px-4 h-full" to="/">Home</g-link>
-        <g-link class="nav-item block md:inline-block px-4 h-full" to="/history">History</g-link>
-        <g-link class="nav-item block md:inline-block px-4 h-full" to="/events">Events</g-link>
-        <g-link class="nav-item block md:inline-block px-4 h-full" to="/team">Team</g-link>
-        <g-link class="nav-item block md:inline-block px-4 h-full" to="/contact">Contact</g-link>
-      </nav>
-    </div>
-  </header>
+  <nav class="flex items-center flex-wrap p-6">
+    <!-- <g-link to="/">
+      <g-image src="../assets/logo-square-white.svg" class="h-auto"></g-image>
+    </g-link> -->
+    <g-link class="mt-1 mr-4" to="/">Home</g-link>
+    <g-link class="mt-1 mr-4" to="/events">Events</g-link>
+    <g-link class="mt-1 mr-4" to="/team">Team</g-link>
+    <g-link class="mt-1 mr-4" to="/contact">Contact</g-link>
+  </nav>
 </template>
-
-<style lang="postcss" scoped>
-.header-dark {
-  @apply bg-white;
-  @apply text-black;
-}
-
-.nav-item {
-  @apply font-bold text-lg;
-  @apply px-4;
-}
-
-header {
-  @apply shadow-lg;
-  position: sticky;
-  top: 0;
-  justify-content: space-between;
-  align-items: center;
-  @apply text-white;
-  @apply bg-black;
-}
-
-.logo {
-  @apply h-12;
-  width: 200px;
-}
-</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,39 +1,62 @@
 <template>
-  <div>
-    <section class="hero h-screen">
-      <div class="md:m-auto md:max-w-3xl md:flex">
-        <div class="flex-1">
-          <img src="../assets/logo-square-white.svg" alt="HackUTD logo" class="">
+  <Layout>
+    <section class="flex flex-col items-center justify-center h-full">
+      <div class="flex flex-col">
+        <div class="flex flex-row mb-20">
+          <div class="flex flex-col justify-evenly mr-20 text-4xl font-display font-bold">
+            <img src="../assets/logo-square-orange.svg" alt="HackUTD logo" width="100%"/>
+            <span>HackUTD</span>
+          </div>
+          <div class="flex flex-col text-6xl font-display font-bold">
+            <span>Hack.</span>
+            <span>Build.</span>
+            <span>Learn.</span>
+          </div>
         </div>
-        <div class="flex-1 text-6xl font-display font-bold px-4">
-          <span class="md:block">Hack.</span>
-          <span class="md:block">Build.</span>
-          <span class="md:block">Learn.</span>
-          <a class="rounded-md shadow-md font-sans font-normal text-lg bg-orange p-4 text-black focus:shadow-lg hover:shadow-lg" href="/">Sign up for our newsletter</a>
+
+        <div class="flex flex-row items-center justify-center">
+          <a
+            class="rounded-md shadow-md font-sans font-normal text-lg bg-orange p-4 text-black focus:shadow-lg hover:shadow-lg"
+            href="/"
+          >Sign up for our newsletter</a>
         </div>
-      </div>
-      <div class="text-center">
-        <span class="material-icons">arrow_down</span>
-        <span class="text-lg mx-2">Learn more</span>
       </div>
     </section>
-  </div>
+    <!-- <section class="flex items-center justify-center h-full">
+      <div class="logo">
+        <img src="../assets/logo-square-white.svg" alt="HackUTD logo" width="100%"/>
+      </div>
+      <div class="flex flex-col text-6xl font-display font-bold px-4">
+        <span>Hack.</span>
+        <span>Build.</span>
+        <span>Learn.</span>
+        <a
+          class="rounded-md shadow-md font-sans font-normal text-lg bg-orange p-4 text-black focus:shadow-lg hover:shadow-lg"
+          href="/"
+        >Sign up for our newsletter</a>
+      </div>
+    </section>-->
+  </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Home',
+    title: "Home",
   },
 };
 </script>
 
 <style lang="postcss">
-body {
-  background-color: #292929;
-  color: white;
+/* .logo {
+  margin-right: 5%;
 }
 
+img {
+  height: 100% !important;
+} */
+
+/*
 .home-links a {
   margin-right: 1rem;
 }
@@ -48,5 +71,5 @@ h1 {
   font-weight: 600;
   text-transform: uppercase;
   @apply text-2xl;
-}
+} */
 </style>
