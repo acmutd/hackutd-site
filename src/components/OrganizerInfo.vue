936ee65abb1e87e455c7c4aeef0e5f39c2c5c548
Willie Chalmers III <willie@williecubed.me>
2020-08-02 11:59:09 -0700

    Clean up OrganizerInfo styles
    Adapt to light/dark theme and use responsive design

--- src/components/OrganizerInfo.vue
+++ src/components/OrganizerInfo.vue
@@ -1,44 +1,118 @@
 <template>
-  <div class="flex bg-white rounded-lg p-6 m-4 shadow-md">
+  <article class="organizer-card">
     <g-image
-      class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
-      :src="'https://www.acmutd.co/png/team-hackutd-camden.jpg'"
+      class="h-16 w-16 md:h-32 md:w-32 rounded-full md:block md:mx-auto md:"
+      :src="imageName"
     />
-    <div class="text-center md:text-left">
-      <h2 class="text-lg text-gray-700">{{ name }}</h2>
-      <div class="text-gray-600">{{ position }}</div>
-      <div class="text-teal-500">{{ bio }}</div>
+    <div class="text-left ml-4">
+      <h1 class="text-xl text-left text-black dark:text-white md:text-center md:text-2xl md:mt-2">{{ name }}</h1>
+      <div class="text-lg text-black md:text-center mb-2 dark:text-white">{{ position }}</div>
+      <div class="text-black dark:text-white">{{ bio }}</div>
       <div class="social-links">
-        <span v-if="github" class="social-link text-gray-500 block md:inline-block md:pr-4">
-          <!-- TODO: Add icon -->
-          <a :href="'https://github.com/' + github">GitHub</a>
+        <span v-if="github" class="social-link">
+          <picture>
+            <source srcset="../assets/social-icons/github-dark.png" media="(prefers-color-scheme: light)">
+            <source srcset="../assets/social-icons/github-light.png" media="(prefers-color-scheme: dark)">
+            <g-image src="../assets/social-icons/github-dark.png" height="16" width="16" class="inline"></g-image>
+          </picture>
+          <a :href="github" class="ml-2">GitHub</a> |
         </span>
-        <div v-if="linkedin" class="social-link text-gray-500 block md:inline-block md:pr-4">
+        <div v-if="linkedin" class="social-link">
           <!-- TODO: Add icon -->
-          <a :href="linkedin">LinkedIn</a>
+          <a :href="linkedin">LinkedIn</a> |
         </div>
-        <div v-if="website" class="social-link text-gray-500 block md:inline-block md:pr-4">
+        <div v-if="website" class="social-link">
           <!-- TODO: Add icon -->
-          <a :href="website">Personal Site</a>
+          <a :href="website">Website</a>
         </div>
       </div>
     </div>
-  </div>
+  </article>
 </template>
 
 <script>
 export default {
-  props: [
-    "name",
-    "bio",
-    "position",
-    "imageUrl",
-    "github",
-    "linkedin",
-    "website"
-  ]
+  props: {
+    /**
+     * This officer's full (first and last) name.
+     */
+    name: String,
+
+    /**
+     * A brief description of this officer.
+     */
+    bio: String,
+
+    /**
+     * The name of this officer's office.
+     * 
+     * Example: "Technical Coordinator"
+     */
+    position: String,
+
+    /**
+     * The filename of this officer's profile image.
+     */
+    imageName: String,
+    
+    /**
+     * A URL to this officer's GitHub profile, if any.
+     */
+    github: String,
+
+    /**
+     * A URL to this officer's LinkedIn profile, if any.
+     */
+    linkedin: String,
+
+    /**
+     * A URL to this officer's personal website, if any.
+     */
+    website: String,
+  },
+  mounted() {
+    const all = document.querySelector('.social-link');
+    console.log(all);
+  },
 };
 </script>
 
-<style>
+<style lang="postcss" scoped>
+.organizer-card {
+  @apply flex;
+  @apply mt-2 p-5;
+  @apply bg-white;
+  @apply rounded-lg shadow-md;
+}
+
+@screen md {
+  .organizer-card {
+    @apply block;
+  }
+}
+
+.social-links {
+  @apply mt-2;
+}
+
+.social-link {
+  @apply text-black text-sm;
+  @apply inline;
+}
+
+@screen dark {
+  .social-link {
+    @apply text-primary;
+  }
+
+  .organizer-card {
+    @apply bg-black;
+  }
+}
+
+@screen md {
+  .social-links {
+    @apply text-center;
+  }
+}
 </style>
