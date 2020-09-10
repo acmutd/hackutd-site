54736e62dfe2106f79dc4545dd60a40185203441
David Richey <darichey1@gmail.com>
2020-09-10 14:48:00 -0500

    Add /sponsor redirect (#71)

---
+++ src/pages/Sponsor.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>Redirecting...</p>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+        meta: [
+            {
+                "http-equiv": "refresh",
+                content: "0; URL=/sponsorship-packet.pdf"
+            }
+        ]
+    }
+  }
+}
+</script>
