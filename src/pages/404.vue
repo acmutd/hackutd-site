82d5553ec170bbd60fbe2481c0efc011709db1bd
MZX102208 <xumichael94@gmail.com>
2020-09-13 21:21:30 -0500

    Make basic 404 page and redirect invalid urls

---
+++ src/pages/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <Layout>
+    <div class="flex h-full w-full justify-center items-center">
+      <div class="flex-col">
+        <h1 class="text-6xl font-bold text-center">404</h1>
+        <h4  class="text-center font-medium text-3xl">Page not found</h4>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<style lang="postcss">
+.prospectus-container {
+  width: 100vw;
+  height: 60vh;
+  padding: 8px;
+}
+
+@media screen and (min-width: 600px) {
+  .prospectus-container {
+    width: 768px;
+    padding: 8px;
+    height: 1024px;
+  }
+}
+</style>
