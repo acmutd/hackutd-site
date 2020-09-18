e1d2748116c6ede4e4ac22e9efc8c44a9256a79f
MZX102208 <xumichael94@gmail.com>
2020-09-18 09:59:30 -0500

    Make basic 404 page and redirect invalid urls (#74)
    * Make basic 404 page and redirect invalid urls
    
    * Remove copied code
    
    * Remove unneccessary 404 redirect

---
+++ src/pages/404.vue
@@ -0,0 +1,10 @@
+<template>
+  <Layout>
+    <div class="flex h-full w-full justify-center items-center">
+      <div class="flex-col">
+        <h1 class="text-6xl font-bold text-center">404</h1>
+        <h4  class="text-center font-medium text-3xl">Page not found</h4>
+      </div>
+    </div>
+  </Layout>
+</template>
